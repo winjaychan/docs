e934bd290b4d3fe8329143634ee7144b57397bb2
winjay <winjay_chan@qq.com>
2024-02-25 00:51:14 +0800

    feat(home): 调整arthas文章

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -84,7 +84,7 @@ export default defineConfig({
   head: [
     [
       'script',
-      { async:'', src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9489861553796481", crossorigin:"anonymous" }
+      { async:"true", src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9489861553796481", crossorigin:"anonymous" }
     ]
   ],
 })
